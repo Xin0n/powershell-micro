9c9baca1f0ea05c208caa0ccb6121fc3e3f2ef7c
Xin0n <108347662+Xin0n@users.noreply.github.com>
2022-06-29 10:34:44 -0300

    Add files via upload

---
+++ VMs con snapshots antiguos incompleto.ps1
@@ -0,0 +1,13 @@
+﻿$vcenter = "vcenter-01.playground.net"
+$user = "nnitto"
+$password = "Wetcom01"
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
+Connect-VIServer -Server $vcenter -User $user -Password $password
+
+$todayDate = Get-Date
+
+$snapshots = Get-VM | Get-Snapshot 
+
+$snapshots | Select-Object VM, created 
