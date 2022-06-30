94587a5e263e5af0e46297d1dc5791cd27db96cc
Xin0n <108347662+Xin0n@users.noreply.github.com>
2022-06-29 21:52:29 -0300

    Add files via upload

---
+++ Datastores.ps1
@@ -0,0 +1,7 @@
+﻿    $user = "nnitto"
+    $paswd = "Wetcom01"
+    $vserver = "vcenter-01.playground.net"
+
+    Connect-VIServer -user $user -password $paswd -server $vserver
+
+    Get-Datastore 
