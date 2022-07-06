1e1356b82e036707792590040cad7c93b7f141bd
Xin0n <108347662+Xin0n@users.noreply.github.com>
2022-07-06 13:01:30 -0300

    Add files via upload

---
+++ Semana 6.ps1
@@ -0,0 +1,11 @@
+﻿$vcenter = "vcenter-01.playground.net"
+$user = "nnitto"
+$password = "Wetcom01"
+
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false
+Connect-VIServer -Server $vcenter -User $user -Password $password
+
+$vm = Get-View -ViewType VirtualMachine -Filter @{"Name" = "VM"}
+$vmhostView = Get-View -ID $vm.Runtime.Host
+$vmhostView.Summary.Runtime
